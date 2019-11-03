84b5cd32719f6fc71dbf9ac83f9804d4b6ed7540
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-11-03 22:36:47 +1000

    gradient method

--- laba1/laba.py
+++ laba1/laba.py
@@ -1,10 +1,12 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
 from sympy.matrices import matrix_multiply_elementwise
 
 n = 6
-np.random.seed(0)
+np.random.seed(5)
 X = sp.MatrixSymbol('x', 6, 1)
 A = np.random.rand(n, n) * 10
 A = sp.Matrix(A @ A.transpose())
@@ -29,17 +31,37 @@ def first_deriv():
     return sp.Matrix(result)
 
 
-if __name__ == '__main__':
+def newtone_method():
+    print("Newton")
     x_k = sp.Matrix(np.random.rand(n))
-
-    # print("end")
-    # val = f.subs({X: x_k})
-    # print(val.evalf())
-
     for i in range(10):
-        print(i)
         x_k = x_k - (second_deriv().subs({X: x_k})**-1).evalf() * first_deriv().subs({X: x_k}).evalf()
-
 
     print(x_k)
     print(f.subs({X: x_k}).evalf())
+
+
+def mag(x):
+    return math.sqrt(sum(i**2 for i in x))
+
+def grad_method():
+    print("Gradient")
+    lamb = 0.001
+    eps = 1e-5
+    x_k = sp.Matrix(np.random.rand(n))
+
+    while True:
+        x_k_p1 = x_k - lamb * first_deriv().subs({X: x_k}).evalf()
+        magnitude = mag(x_k_p1 - x_k)
+        print(magnitude)
+        if magnitude < eps:
+            print(magnitude)
+            print(x_k_p1)
+            print(f.subs({X: x_k_p1}).evalf())
+            break
+        x_k = x_k_p1
+
+
+if __name__ == '__main__':
+    newtone_method()
+    grad_method()
